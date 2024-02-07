2eb42fbe5b48b8b825b2771c3f04b5483727898a
bee <104741505+TheBeesSneeze@users.noreply.github.com>
2024-02-07 13:40:10 -0600

    Death Plane done

--- Assets/Scripts/Deathplane.cs
+++ Assets/Scripts/Deathplane.cs
@@ -1,15 +1,45 @@
+/*******************************************************************************
+ * File Name :         DeathPlane.cs
+ * Author(s) :         Jamison Parks, Toby Schamberger
+ * Creation Date :     2/7/2024
+ *
+ * Brief Description : Resets the scene when collides with player. Sets Interractable
+ * Objects back to their starting position.
+ *****************************************************************************/
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Deathplane : MonoBehaviour
+public class DeathPlane : MonoBehaviour
 {
-    /*private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
-        if (collision.tag.Player)
+        PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+        if(player != null)
         {
-            SceneManger.LoadScene(0);
+            OnPlayerCollision(player);
+            return;
         }
-    }*/
+
+        InterractableObject obj = collision.gameObject.GetComponent<InterractableObject>();
+        if (obj != null)
+        {
+            OnObjectCollision(obj);
+            return;
+        }
+    }
+
+    private void OnPlayerCollision(PlayerController player)
+    {
+        Debug.Log("player died. reload scene!");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void OnObjectCollision(InterractableObject obj)
+    {
+        obj.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        obj.transform.position = obj.StartingPosition;
+    }
 }
